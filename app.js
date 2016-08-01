baa7ba249766f5c838857421866b0729ec95585a
Yeiber Cano <yeibercano@gmail.com>
2016-08-01 04:38:44 -0700

    create books route and serve response to client

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.get('/', function(req, res){
 	res.send('made it to home page')
 });
 
+app.get('/books', function(req, res) {
+	Book.find({})
+	.exec(function(err, books) {
+		if (err) {
+			res.send(err)
+		} else {
+			console.log('books', books)
+			res.json(books)
+		}
+	});
+})
+
 app.listen(port, function(){
 	console.log('app listening on: ' + port)
 });
