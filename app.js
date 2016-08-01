cb7342a1e672a6fc89a0b9d51f622f657db9a2b3
Yeiber Cano <yeibercano@gmail.com>
2016-08-01 04:31:45 -0700

    add home page access

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express'),
 		bodyParser = require('body-parser'),
 		mongoose = require('mongoose');
 var Book = require('./Book.model.js');
-
+var port = 4444;
 var db = 'mongodb://localhost/crud';
+
 mongoose.connect(db);
 
-var port = 4444;
+// routes
+app.get('/', function(req, res){
+	res.send('made it to home page')
+});
 
 app.listen(port, function(){
 	console.log('app listening on: ' + port)
